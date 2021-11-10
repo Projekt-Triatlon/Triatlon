9bc387c1a9d843d95bae9170a7e58fe238bd06ab
gyorvaripeter <gyorvaripeter1996@gmail.com>
2021-11-10 19:10:20 +0100

    handier db connection w\ docker

--- TriatlonDataAccess/Context/TriatlonDbConfiguration.cs
+++ TriatlonDataAccess/Context/TriatlonDbConfiguration.cs
@@ -8,7 +8,7 @@ namespace TriatlonDataAccess.Context
 	{
 		public static string GetConnectionString()
 		{
-			return @"Data Source=localhost\;Initial Catalog=TriatlonDb;Integrated Security=True;";
+			return @"Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true";
 		}
 	}
 }

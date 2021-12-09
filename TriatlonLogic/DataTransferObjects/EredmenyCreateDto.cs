bb05738c8651d28be8f08c5ef40cf98a008b8964
Kreutzog András <kreutzoga@gmail.com>
2021-12-09 14:43:39 +0100

    Fix bugs

--- TriatlonLogic/DataTransferObjects/EredmenyCreateDto.cs
+++ TriatlonLogic/DataTransferObjects/EredmenyCreateDto.cs
@@ -12,27 +12,36 @@ namespace TriatlonLogic.DataTransferObjects
 	{
 		public long OID { get; set; }
 
+		[DisplayName("Chipkód")]
 		public string ChipKod { get; set; }
 
+		[DisplayName("Rajtszám")]
 		public long Rajtszam { get; set; }
 
+		[DisplayName("Úszás idő")]
 		public TimeSpan UszasIdo { get; set; }
 
+		[DisplayName("Depo 1 idő")]
 		public TimeSpan Depo1Ido { get; set; }
 
+		[DisplayName("Kerékpár idő")]
 		public TimeSpan KerekparIdo { get; set; }
 
+		[DisplayName("Depo 2 idő")]
 		public TimeSpan Depo2Ido { get; set; }
 
+		[DisplayName("Futás idő")]
 		public TimeSpan FutasIdo { get; set; }
 
+		[DisplayName("Cél idő")]
 		public TimeSpan CelIdo { get; set; }
 
-
+		[DisplayName("Versenyző")]
 		public long VersenyzoOID { get; set; }
 
 		public List<SelectListItem> VersenyzoSelectList { get; set; }
 
+		[DisplayName("Verseny")]
 		public long VersenyOID { get; set; }
 
 		public List<SelectListItem> VersenySelectList { get; set; }
